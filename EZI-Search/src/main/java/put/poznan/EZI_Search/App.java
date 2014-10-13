2fb4bd7ca2ebd53e24988c70295c8dee48780a0e
Tomasz Straszewski <straszewski.tomasz@gmail.com>
2014-10-13 20:10:25 +0200

    init

---
+++ EZI-Search/src/main/java/put/poznan/EZI_Search/App.java
@@ -0,0 +1,13 @@
+package put.poznan.EZI_Search;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ EZI-Search/src/main/java/put/poznan/EZI_Search/tfidf/Document.java
@@ -0,0 +1,10 @@
+package put.poznan.EZI_Search.tfidf;
+
+public class Document {
+
+	private int id;
+	private String titile;
+	private String content;
+	
+	
+}
