3e9ebe33213b2857b1e32a1e075f0e961b15d8a0
tim91 <straszewski.tomasz@gmail.com>
2014-11-18 18:01:28 +0100

    Ze zmiana lambdy

--- LinearRegression/src/main/java/pl/tstraszewski/LinearRegression.java
+++ LinearRegression/src/main/java/pl/tstraszewski/LinearRegression.java
@@ -15,44 +15,67 @@ public class LinearRegression {
     public static void main(String [] args){
 
         try {
-            Pair<Matrix,Matrix> matrixes = InputDataReader.readData("src/main/resources/DenBosch_train.data");
+        	
+        	for(int lambda = -3; lambda<=25; lambda++){
+        		Pair<Matrix,Matrix> matrixes = InputDataReader.readData("src/main/resources/DenBosch_train.data");
 
-            Matrix X = matrixes.getKey();
-            Matrix Y = matrixes.getValue();
+                Matrix X = matrixes.getKey();
+                Matrix Y = matrixes.getValue();
 
-            Matrix XT = X.transpose();
-            Matrix H = XT.times(X);
+                Matrix XT = X.transpose();
+                Matrix H = XT.times(X);
 
-            if(H.det() < Math.pow(10,-8)){
-                //Osobliwa
-            	System.out.println("Osobliwy");
-                Matrix I = Matrix.identity(H.getRowDimension(),H.getRowDimension()).times(Math.pow(10,-5));
-                H = H.plus(I);
-            }
+//                if(H.det() < Math.pow(10,-8)){
+                    //Osobliwa
+//                	System.out.println("Osobliwy");
+                    Matrix I = Matrix.identity(H.getRowDimension(),H.getRowDimension()).times(Math.pow(2, lambda));
+                    H = H.plus(I);
+//                }
 
-            Matrix G = XT.times(Y);
-            Matrix W = H.inverse().times(G);
+                Matrix G = XT.times(Y);
+                Matrix W = H.inverse().times(G);
 
-            W.print(1,6);
+//                W.print(1,6);
 
-            //check
-            Pair<Matrix,Matrix> matrixes1 = InputDataReader.readData("src/main/resources/DenBosch_train.data");
-            Matrix Xtest = matrixes1.getKey();
-            Matrix Ytest = matrixes1.getValue();
+                //check
+                Pair<Matrix,Matrix> matrixes1 = InputDataReader.readData("src/main/resources/DenBosch_test.data");
+                Matrix Xtest = matrixes1.getKey();
+                Matrix Ytest = matrixes1.getValue();
+                
+                Matrix Ycounted = Xtest.times(W);
+                
+                Matrix Ydiff = Ytest.minus(Ycounted);
+                
+                double sum=0.0;
+                for(int i=0; i < Ydiff.getRowDimension(); i++){
+                	sum += Math.pow(Ydiff.get(i, 0),2);
+                }
+                
+                sum = sum / Xtest.getRowDimension();
+                
+//                Ydiff.print(1, 6);
+                System.out.println("Test Result: " + Math.sqrt(sum) + " lambda: " + Math.pow(2, lambda));
+                
+                matrixes1 = InputDataReader.readData("src/main/resources/DenBosch_train.data");
+                Xtest = matrixes1.getKey();
+                Ytest = matrixes1.getValue();
+                
+                Ycounted = Xtest.times(W);
+                
+                Ydiff = Ytest.minus(Ycounted);
+                
+                sum=0.0;
+                for(int i=0; i < Ydiff.getRowDimension(); i++){
+                	sum += Math.pow(Ydiff.get(i, 0),2);
+                }
+                
+                sum = sum / Xtest.getRowDimension();
+                
+//                Ydiff.print(1, 6);
+                System.out.println("Train Result: " + Math.sqrt(sum) + " lambda: " + Math.pow(2, lambda));
+        	}
+        	
             
-            Matrix Ycounted = Xtest.times(W);
-            
-            Matrix Ydiff = Ytest.minus(Ycounted);
-            
-            double sum=0.0;
-            for(int i=0; i < Ydiff.getRowDimension(); i++){
-            	sum += Math.pow(Ydiff.get(i, 0),2);
-            }
-            
-            sum = sum / Xtest.getRowDimension();
-            
-            Ydiff.print(1, 6);
-            System.out.println("Result: " + Math.sqrt(sum));
         } catch (Exception e) {
             e.printStackTrace();
         }
