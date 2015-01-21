12b11664035085dc4f8ee7205f4d7f94c6243e4e
Tomasz Straszewski <straszewski.tomasz@gmail.com>
2015-01-21 17:37:36 +0100

    dfgdfg

--- LinearRegression/src/main/java/pl/straszewskiRosolak/MainApplication.java
+++ LinearRegression/src/main/java/pl/straszewskiRosolak/MainApplication.java
@@ -1,8 +1,10 @@
 package pl.straszewskiRosolak;
 
-import pl.straszewskiRosolak.LogisticRegression.Report.TestingReport;
-import pl.straszewskiRosolak.LogisticRegression.Report.TrainingReport;
-import pl.straszewskiRosolak.LogisticRegression.StochasticLogisticRegression;
+import pl.straszewskiRosolak.Regression.NewtonRaphsonRegression;
+import pl.straszewskiRosolak.Regression.Regression;
+import pl.straszewskiRosolak.Regression.Report.TestingReport;
+import pl.straszewskiRosolak.Regression.Report.TrainingReport;
+import pl.straszewskiRosolak.Regression.StochasticRegression;
 
 /**
  * Created by Tomek on 2014-12-17.
@@ -11,22 +13,22 @@ public class MainApplication {
 
     public static void main(String [] args){
 
-//        LogisticRegression lr = new LogisticRegression();
-//        TrainingReport trainReport = lr.train("src/main/resources/file3_train.txt");
-//        System.out.println(trainReport.toString());
-//        TestingReport testReport = lr.test("src/main/resources/file3_train.txt");
-//        System.out.println(testReport.toString());
-//        TestingReport testReport1 = lr.test("src/main/resources/file3_test.txt");
-//        System.out.println(testReport1.toString());
-//        lr.getW().print(1,6);
+        Regression lr = new NewtonRaphsonRegression();
+        TrainingReport trainReport = lr.train("src/main/resources/file3_train.txt");
+        System.out.println(trainReport.toString());
+        TestingReport testReport = lr.test("src/main/resources/file3_train.txt");
+        System.out.println(testReport.toString());
+        TestingReport testReport1 = lr.test("src/main/resources/file3_test.txt");
+        System.out.println(testReport1.toString());
+        lr.getW().print(1,6);
 
 
-        StochasticLogisticRegression slr = new StochasticLogisticRegression();
-        TrainingReport strainReport = slr.train("src/main/resources/file3_train.txt");
-        System.out.println(strainReport.toString());
-        TestingReport stestReport = slr.test("src/main/resources/file3_train.txt");
-        System.out.println(stestReport.toString());
-        TestingReport stestReport1 = slr.test("src/main/resources/file3_test.txt");
-        System.out.println(stestReport1.toString());
+//        StochasticRegression slr = new StochasticRegression();
+//        TrainingReport strainReport = slr.train("src/main/resources/file3_train.txt");
+//        System.out.println(strainReport.toString());
+//        TestingReport stestReport = slr.test("src/main/resources/file3_train.txt");
+//        System.out.println(stestReport.toString());
+//        TestingReport stestReport1 = slr.test("src/main/resources/file3_test.txt");
+//        System.out.println(stestReport1.toString());
     }
 }
